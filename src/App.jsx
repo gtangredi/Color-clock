c78a595ef2092b85fe5a3d63b9c382af566acdd6
gtangredi <gtangredi315@gmail.com>
2025-11-22 20:55:57 -0500

    Update project files and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import { format } from 'date-fns';
+import React from "react";
+import blogData from "../data/blog.js";
+import Header from "./Header";
+import About from "./About";
 
-function App() {
-  const [ color, setColor ] = useState('#fff');
-  let date = new Date();
-  
-  function handleColorChange(e) {
-    console.log('handleColorChange', e.target.value);
-    setColor(e.target.value);
-  }
-    setInterval(() => {
-      setDate(new Date());
-    }, 1000) ;
+console.log(blogData);
 
-    return (
-    <>
-      <div>
-        <label htmlFor="colorPicker">Color: </label>
-        <input type="color" id="colorPicker" onChange={ handleColorChange } />
-      </div>
-      <hr />
-      <p className="clock" style={ {} }>{ format(date, 'MM/dd/yyyy HH:mm:ss:SSS') }</p>
-    </>
-  )
+function App () {
+  return (
+    <div classNAme="App">
+      <Header />
+      <About />
+      <ArticleList />
+
+    </div>
+  );
 }
 
-export default App
+export default App;
 
 
  
---
+++ src/components/Article List.jsx
@@ -0,0 +1,13 @@
+import Article from "./Article"
+
+export default function ArticleList(props) {
+    return (
+     <>
+        (ul)
+            <Article />
+            <Article />
+            <Article />
+        </ul>
+     </>
+ )
+}
